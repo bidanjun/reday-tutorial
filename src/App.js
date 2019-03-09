8efdf4c31a9ea24b6abb82bba0a57c74e781b593
bidanjun <bidanjun@gmail.com>
2019-03-09 15:04:28 +0800

    Counter demo use react hook itself

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+//1. 创建自定义的hook
+//一般原则：如果返回值是两个，可以考虑返回数组，这样使用时可以自行命名，类似useState
+//若大于两个，由于顺序问题难以记忆，我们应如下方，返回object
+const useCounter = initialCount => {
+  const [count, setCount] = useState(initialCount);
+  return {
+    value: count,
+
+    //演示需要参数的函数
+    increase:(step)=> () => setCount(prevCount => prevCount + step),
+    
+    //演示不需要参数的函数
+    reset: () => setCount(initialCount)
+  };
+};
+
+//1. 创建counter组件
+function Counter({ initialCount,step }) {
+
+  //问题1 违背原则：组件仅根据传入的属性呈现
+  const counter = useCounter(initialCount);
+  return (
+    <div>
+    <p>step ={step} You clicked {counter.value} times</p>
+    <button onClick={counter.increase(step)}>
+      Click me
+    </button>
+    <button onClick={counter.reset}>Reset</button>
+  </div>
+  );
 }
 
-export default App;
+//问题2 自定义hook复用useCounter逻辑,而不是共享实例，我们有时需要共享实例
+//两个Counter组件，行为是独立的
+//因此这里共享的是逻辑，而不是实例
+export default function App() {
+  return (
+    <div >
+      <h1>Counter use React hook </h1>
+      <Counter initialCount={0} step={2}/>
+      <Counter initialCount={0} step={1}/>
+    </div>
+  );
+}
